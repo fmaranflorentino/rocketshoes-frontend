c7e17270b3b87ee09b38ff0bcadde781b5571a5a
Flávio Florentino <florentino.flavioweb@gmail.com>
2020-03-16 18:35:56 -0300

    feat(components/Header): Create Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import Header from './components/Header';
+
 import GlobalStyles from './styles/global';
 
 import Routes from './routes';
@@ -8,6 +10,7 @@ import Routes from './routes';
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
       <GlobalStyles />
     </BrowserRouter>
